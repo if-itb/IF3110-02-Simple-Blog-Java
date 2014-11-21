964df33b7fc8d1fbd031242695a8d9038a97c116
imballinst <lucent_rays@yahoo.com>
2014-11-22 05:12:41 +0700

    deletePost, publishPost

--- src/java/source/Post.java
+++ src/java/source/Post.java
@@ -8,6 +8,9 @@ package source;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Try Ajitiono
@@ -55,50 +58,84 @@ public class Post {
     /**
      * Mengubah status post di database dari unpublished menjadi published
      * @param post_ID post id di database
+     * @throws java.sql.SQLException
      */
     public void publishPost(int post_ID) throws SQLException {
         //login database
         KoneksiDatabase.setUser("root");
         KoneksiDatabase.setPassword("akhfa");
         KoneksiDatabase.setDatabase("localhost","blog");
-        //statement
-        Connection koneksi = KoneksiDatabase.getKoneksi();
-        Statement statement = koneksi.createStatement();
-        //query
-        String queryPublishPost = "UPDATE posts SET published=1";
-        //execute query
-        statement.executeUpdate(queryPublishPost);
-        //tutup koneksi database
-        koneksi.close();
+        try ( //statement
+            Connection koneksi = KoneksiDatabase.getKoneksi()) {
+            Statement statement = koneksi.createStatement();
+            //query
+            String queryPublishPost = "UPDATE posts SET published=1";
+            //execute query
+            statement.executeUpdate(queryPublishPost);
+            //tutup koneksi database
+            koneksi.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     /**
      * Mengubah aspek dari post di database
      * @param post_ID post id di database
+     * @param title input judul baru dari pengguna
+     * @param date input tanggal baru dari pengguna
+     * @param content input konten baru dari pengguna
+     * @throws java.sql.SQLException
      */
-    public void editPost(int post_ID) {
-        
+    public void editPost(int post_ID, String title, String date, String content) throws SQLException {
+        //login database
+        KoneksiDatabase.setUser("root");
+        KoneksiDatabase.setPassword("akhfa");
+        KoneksiDatabase.setDatabase("localhost","blog");
+        try ( //statement
+            Connection koneksi = KoneksiDatabase.getKoneksi()) {
+            Statement statement = koneksi.createStatement();
+            //query
+            String queryEditTitle = "UPDATE posts SET title='" + title + "' WHERE post_id=" + post_ID;
+            String queryEditDate = "UPDATE posts SET date='" + date + "' WHERE post_id=" + post_ID;
+            String queryEditContent = "UPDATE posts SET content='" + content + "' WHERE post_id=" + post_ID;
+            //execute query
+            statement.executeUpdate(queryEditTitle);
+            statement.executeUpdate(queryEditDate);
+            statement.executeUpdate(queryEditContent);
+            //tutup koneksi database
+            koneksi.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     /**
      * Menghapus post dari database
      * @param post_ID post id di database
+     * @throws java.sql.SQLException
      */
     public void deletePost(int post_ID) throws SQLException {
         //login database
         KoneksiDatabase.setUser("root");
         KoneksiDatabase.setPassword("akhfa");
         KoneksiDatabase.setDatabase("localhost","blog");
-        //statement
-        Connection koneksi = KoneksiDatabase.getKoneksi();
-        Statement statement = koneksi.createStatement();
-        //query
-        String queryDeletePost = "DELETE FROM posts WHERE post_id=" + post_ID;
-        String queryDeleteComments = "DELETE FROM comments WHERE post_id=" + post_ID;
-        //execute query
-        statement.executeUpdate(queryDeletePost);
-        statement.executeUpdate(queryDeleteComments);
-        //tutup koneksi database
-        koneksi.close();
+        try ( //statement
+            Connection koneksi = KoneksiDatabase.getKoneksi()) {
+            Statement statement = koneksi.createStatement();
+            //query
+            String queryDeletePost = "DELETE FROM posts WHERE post_id=" + post_ID;
+            String queryDeleteComments = "DELETE FROM comments WHERE post_id=" + post_ID;
+            //execute query
+            statement.executeUpdate(queryDeletePost);
+            statement.executeUpdate(queryDeleteComments);
+            //tutup koneksi database
+            koneksi.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
