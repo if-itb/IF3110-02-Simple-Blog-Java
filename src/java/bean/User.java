af7394ffa14d1ee9a35aaabe8524ca0a03d08243
Timothy Pratama <timothy.pratama@gmail.com>
2014-11-22 03:58:36 +0700

    +Bisa login & register +email di validasi pake javascript +register usernamenya harus unik +validasi login username dan password *masih belum menggunakan cookie

--- src/java/bean/User.java
+++ src/java/bean/User.java
@@ -1,5 +1,6 @@
 package bean;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,8 +9,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name ="user",eager = true)
 @SessionScoped
@@ -19,6 +24,7 @@ public class User implements Serializable
     private String password;
     private String email;
     private String role;
+    private List<User> daftar_user;
     
     public User() 
     {
@@ -26,6 +32,7 @@ public class User implements Serializable
         password="";
         role="";
         email="";
+        daftar_user = new ArrayList<>();
     }
 
     public String getRole() {
@@ -59,14 +66,69 @@ public class User implements Serializable
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<User> getDaftar_user() {
+        fetchUsersFromDB();
+        return daftar_user;
+    }
     
-    public List<User> getUsersFromDB()
+    public void logout()
+    {
+        System.out.println("logout");
+        username="";
+        email="";
+        role="";
+        password="";
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        try 
+        {
+            context.redirect(context.getRequestContextPath() + "/index.xhtml");
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public void register()
+    {
+        System.out.println("register");
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/simple_blog_java", "root","");
+            System.out.println("connected to database!");
+            String query = "INSERT INTO user (username, password, email, role) VALUES (?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, email);
+            ps.setString(4, role);
+            int executeUpdate = ps.executeUpdate();
+            if(executeUpdate > 0)
+            {
+                System.out.println("update succesful");
+            }
+            else
+            {
+                System.out.println("update failure");
+            }
+            con.close();
+        } 
+        catch (InstantiationException | IllegalAccessException | SQLException | ClassNotFoundException ex) 
+        {
+            System.out.println(ex.toString());
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public void fetchUsersFromDB()
     {
         String url = "jdbc:mysql://localhost/simple_blog_java";
         String username = "root";
         String password = "";
         Connection con = null;
-        List<User> records = new ArrayList<>();
+        daftar_user = new ArrayList<>();
         try 
         {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -80,11 +142,7 @@ public class User implements Serializable
                 temp.setEmail(rs.getString("email"));
                 temp.setPassword(rs.getString("password"));
                 temp.setRole(rs.getString("role"));
-                records.add(temp);
-                System.out.println(temp.getUsername());
-                System.out.println(temp.getPassword());
-                System.out.println(temp.getPassword());
-                System.out.println(temp.getRole());
+                daftar_user.add(temp);
             }
             con.close();
         } 
@@ -93,6 +151,66 @@ public class User implements Serializable
             System.out.println(ex.toString());
             System.out.println(ex.getMessage());
         }
-        return records;
+    }
+    
+    private void login()
+    {
+        System.out.println("Login");
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        try 
+        {
+            context.redirect(context.getRequestContextPath() + "/" + role + ".xhtml");
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public String getAvail()
+    {
+        System.out.println("check Available");
+        Boolean available = true;
+        String result = "";
+        fetchUsersFromDB();
+        for (User daftar_user1 : daftar_user) {
+            if (daftar_user1.getUsername().equals(username)) {
+                result = "<div class=\"alert alert-danger login-alert\">username already exist</div>\n";
+                available = false;
+                break;
+            }
+        }
+        if(available)
+        {
+            register();
+            login();
+        }
+        return result;
+    }
+    public String getAuth()
+    {
+        System.out.println("auth");
+        String output ="";
+        Boolean match = false;
+        fetchUsersFromDB();
+        for(User user : daftar_user)
+        {
+            if(user.getUsername().equals(username) && user.getPassword().equals(password))
+            {
+                match = true;
+                role = user.role;
+                email = user.email;
+                break;
+            }
+        }
+        if(match)
+        {
+            login();
+        }
+        else
+        {
+            output = "<div class=\"alert alert-danger login-alert\">wrong username or password</div>\n";
+        }
+        return output;
     }
 }
