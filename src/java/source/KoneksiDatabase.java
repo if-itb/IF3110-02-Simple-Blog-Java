6dffa0c611f14c8d6277e58274be12b12f682d4b
akhfa <akhmadfakhoni@gmail.com>
2014-11-22 03:30:19 +0700

    Improvement di Koneksi database agar tidak lagi setUrl manual

--- src/java/source/KoneksiDatabase.java
+++ src/java/source/KoneksiDatabase.java
@@ -7,6 +7,8 @@ public class KoneksiDatabase {
     private static Connection koneksi;
     private static String user;
     private static String password;
+    private static String url;
+    
     
     public static void setUser(String _user)
     {
@@ -19,13 +21,42 @@ public class KoneksiDatabase {
     }
     
     /**
+     * Fungsi untuk memasukkan nama database
+     * @param _databaseName 
+     */
+    public static void setDatabase(String _databaseName)
+    {
+        url = "jdbc:mysql://localhost:3306/" + _databaseName;
+    }
+    
+    /**
+     * Fungsi untuk mengeset database jika domain bukan di localhost, namun port masih 3306
+     * @param _domain
+     * @param _databaseName 
+     */
+    public static void setDatabase(String _domain, String _databaseName)
+    {
+        url = "jdbc:mysql://"+ _domain +":3306/" + _databaseName;
+    }
+    
+    /**
+     * Fungsi untuk mengeset database jika domain bukan localhost dan port bukan 3306
+     * @param _domain
+     * @param _port
+     * @param _databaseName 
+     */
+    public static void setDatabase(String _domain, String _port, String _databaseName)
+    {
+        url = "jdbc:mysql://"+ _domain +":"+ _port +"/" + _databaseName;
+    }
+    
+    /**
      * Fungsi untuk mendapatkan koneksi ke database. User dan password dari database harus diset terlebih dahulu.
      * @return Mengembalikan koneksi ke database.
      * @throws java.sql.SQLException
      */
     public static Connection getKoneksi() throws SQLException{
         if(koneksi == null && user != null && password != null) {
-                String url = "jdbc:mysql://localhost:3306/simpleblogdb";
                 DriverManager.registerDriver(new com.mysql.jdbc.Driver());
                 koneksi = DriverManager.getConnection(url, user, password);
                 System.out.println("Berhasil terkoneksi");
