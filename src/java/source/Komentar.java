12bff2000c39370983bfd03cefca66292e9b4473
Rakhmatullah Yoga S <Rakhmatullah Yoga S@rakhmatullah25>
2014-11-22 05:08:52 +0700

    nambah kelas komentar

---
+++ src/java/source/Komentar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package source;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Rakhmatullah Yoga S
+ */
+public class Komentar extends Post {
+    private static String Nama;
+    private static String Email;
+    private static String Content;
+    private static Date Tanggal;
+}
