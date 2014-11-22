fa02f63f5594567d189adf33714f142b9a89bf61
Rakhmatullah Yoga S <13512053@std.stei.itb.ac.id>
2014-11-22 22:18:26 +0700

    full feature, not implemented yet

--- src/java/source/Komentar.java
+++ src/java/source/Komentar.java
@@ -7,9 +7,10 @@
 package source;
 
 import java.sql.Connection;
-import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 
 /**
  *
@@ -18,26 +19,63 @@ import java.sql.Statement;
 public class Komentar extends Post {
     private static String Nama;
     private static String Email;
-    private static String Content;
-    private static Date Tanggal;
+    private static String Comment;
+    private static Timestamp Time;
     
     /**
      * Menambahkan komentar ke dalam basis data
      * @param ID 
+     * @return  HTML script untuk memuat komentar yang akan digunakan oleh AJAX
      * @throws java.sql.SQLException 
      */
-    public static void AddComment(int ID) throws SQLException {
+    public static String LoadComment(int ID) throws SQLException {
         //login database
+        String HTMLcode = "";
         KoneksiDatabase.setUser("root");
         KoneksiDatabase.setPassword("");
         KoneksiDatabase.setDatabase("localhost","simpleblogdb");
-        Connection koneksi = KoneksiDatabase.getKoneksi();
-        //Statement statement = koneksi.createStatement();
-        //String QueryAddComment = "";
+        try (Connection koneksi = KoneksiDatabase.getKoneksi()) {
+            Statement statement = koneksi.createStatement();
+            String QueryLoadComment = "SELECT * FROM komentar WHERE ID="+ID+" ORDER BY Waktu ASC";
+            ResultSet result = statement.executeQuery(QueryLoadComment);
+            while(result.next()) {
+                Nama = result.getString("Nama");
+                Email = result.getString("Email");
+                Comment = result.getString("Komentar");
+                Time = result.getTimestamp("Waktu");
+                HTMLcode +=
+                        "    <li class=\"art-list-item\">\n" +
+                        "        <div class=\"art-list-item-title-and-time\">\n" +
+                        "            <h2 class=\"art-list-title\">" + Nama + "</h2>\n" +
+                        "            <div class=\"art-list-time\">" + Time.toString() + "</div>\n" +
+                        "        </div>\n" +
+                        "        <p>" + Comment + "</p>\n" +
+                        "    </li>";
+            }
+        }
+        return HTMLcode;
+    }
+    /**
+     * 
+     * @param nama
+     * @param email
+     * @param comment
+     * @param time
+     * @throws SQLException 
+     */
+    public static void AddComment(String nama, String email, String comment, Timestamp time) throws SQLException {
+        String InsertQuery;
+        KoneksiDatabase.setUser("root");
+        KoneksiDatabase.setPassword("");
+        KoneksiDatabase.setDatabase("localhost","simpleblogdb");
+        try (Connection koneksi = KoneksiDatabase.getKoneksi()) {
+            Statement statement = koneksi.createStatement();
+            InsertQuery = "INSERT INTO komentar (ID, Nama, Email, Komentar) VALUES ('" + Post.idPost + "', '" + nama + "', '" + email + "', '" + comment + "')";
+        }
     }
     
     public String GetComment() {
-        return Content;
+        return Comment;
     }
     public String GetName() {
         return Nama;
@@ -45,8 +83,11 @@ public class Komentar extends Post {
     public String GetEmail() {
         return Email;
     }
+    public Timestamp GetTime() {
+        return Time;
+    }
     
     public static void main(String[] args) throws SQLException {
-        Komentar.AddComment(31);
+        System.out.println(Komentar.LoadComment(31));
     }
 }
