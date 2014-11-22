721056863e9184fe3d5d7e545eacf86feef48829
eric2861994 <eric2861994@gmail.com>
2014-11-22 18:47:49 +0700

    Kok Error?

--- src/somepackage/DatabaseUtility.java
+++ src/somepackage/DatabaseUtility.java
@@ -1,21 +1,59 @@
 package somepackage;
 
+import java.sql.*;
+import java.util.*;
+
 public class DatabaseUtility {
 	private static DatabaseUtility instance = new DatabaseUtility();
+	Connection con;
 
 	public UserDetails findUser(String username, String password) {
-		if (username.equals("secret") && password.equals("secret"))
-			return new UserDetails("Daniel Ishutin", "dendimon@gmail.com");
-		
-		return null;
+		ResultSet rs = null;
+		PreparedStatement pst = null;
+
+		if (con == null)
+			con = getConnection();
+
+		String stm = "Select * from user";
+		UserDetails result = null;
+		try {
+			pst = con.prepareStatement(stm);
+			pst.execute();
+			rs = pst.getResultSet();
+
+			while (rs.next()) {
+				if (rs.getString(2).equals(username)
+						&& rs.getString(3).equals(password))
+					result = new UserDetails(rs.getString(4), rs.getString(5));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
-	
+
+	private Connection getConnection() {
+		Connection connection = null;
+
+		String url = "jdbc:mysql://localhost:3306/wbd_db";
+		String user = "WBD_USER";
+		String password = "QKC3zwhJ";
+
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection completed");
+		} catch (SQLException exception) {
+			System.out.println("Connection FAILEDDDDD!!!!");
+			System.out.println(exception.getMessage());
+		}
+		return connection;
+	}
+
 	private DatabaseUtility() {
 	}
 
 	public static DatabaseUtility getInstance() {
 		return instance;
 	}
-	
-	
+
 }
