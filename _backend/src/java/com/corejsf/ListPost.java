dfacfadacd168dde1efe830cd63c6c345fd6112d
indmhmmd <indam.muhammad@gmail.com>
2014-11-24 19:14:58 +0700

    List post sudah, add post masih belum

---
+++ _backend/src/java/com/corejsf/ListPost.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf;
+
+import java.sql.*;
+import java.util.*;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "listpost")
+@RequestScoped
+
+/**
+ *
+ * @author Indam Muhammad
+ */
+public class ListPost {
+    private ArrayList<Post> posts;
+    public ListPost(){
+        posts = new ArrayList<Post>();
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/simpleblog";
+        String user = "root";
+        String driver = "com.mysql.jdbc.Driver";
+        String password = "";
+        try {
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(url, user, password);
+            Statement sm = con.createStatement();
+            ResultSet res = sm.executeQuery("SELECT * FROM post ORDER BY Tanggal DESC");
+            while(res.next()){
+                Post pos = new Post();
+                pos.setId(res.getInt("id"));
+                pos.setJudul(res.getString("Judul"));
+                pos.setKonten(res.getString("Konten"));
+                pos.setStatus(res.getString("Status"));
+                pos.setTanggal(res.getDate("Tanggal"));
+                posts.add(pos);
+            }
+            con.close();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        finally{
+        }
+    }
+    
+    public ArrayList<Post> getPosts(){
+        return this.posts;
+    }
+    
+}
--- _backend/src/java/com/corejsf/Post.java
+++ _backend/src/java/com/corejsf/Post.java
@@ -6,6 +6,13 @@
 
 package com.corejsf;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -19,7 +26,7 @@ public class Post {
     private int id;
     private String status;
     private String judul;
-    private String tanggal;
+    private Date tanggal;
     private String konten;
     
     public Post() {
@@ -37,7 +44,7 @@ public class Post {
         return judul;
     }
     
-    public String getTanggal(){
+    public Date getTanggal(){
         return tanggal;
     }
     
@@ -57,11 +64,43 @@ public class Post {
         this.judul = judul;
     }
     
-    public void setTanggal(String tanggal){
+    public void setTanggal(Date tanggal){
         this.tanggal = tanggal;
     }
     
     public void setKonten(String konten){
         this.konten = konten;
+    }
+    public void addPost(String judul, String tanggal, String konten, String status){
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/simpleblog";
+        String user = "root";
+        String driver = "com.mysql.jdbc.Driver";
+        String password = "";
+        try {
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(url, user, password);
+            String query = "INSERT INTO post (id_member, Status,Judul,Konten,Tanggal) "
+                    + "VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, 1);            
+            ps.setString(2, status);
+            ps.setString(3, judul);
+            ps.setString(4, konten);
+            ps.setDate(5, new Date(115, 1, 1));            
+            ps.executeUpdate();
+            con.close();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        finally{
+        }
+    }
+    public Date aturTanggal(String s){
+        int a = Integer.parseInt(s.substring(0, 3));
+        int b = Integer.parseInt(s.substring(5, 6));
+        int c = Integer.parseInt(s.substring(8, 9));
+        Date d = new Date(a,b,c);
+        return d;
     }
 }
