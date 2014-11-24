c880c95d4e70394b66ebe8e52132b099deb6a2e5
Ichlasul Amal <ichlasul.a@gmail.com>
2014-11-25 00:09:55 +0700

    Add post entity

---
+++ SimpleBlog/src/java/id/ac/itb/informatika/wbd/jpa/entities/Post.java
@@ -0,0 +1,134 @@
+package id.ac.itb.informatika.wbd.jpa.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "posts")
+public class Post implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Basic(optional = false)
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "post_title")
+    private String title;
+    
+    @Column(name = "post_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+    
+    @Column(name = "post_content")
+    private String content;
+    
+    @Column(name = "post_featured")
+    private Boolean featured;
+    
+    @Column(name = "post_modified")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modified;
+    
+    public Post() {
+    }
+
+    public Post(Long id) {
+        this.id = id;
+    }
+
+    public Post(Long id, String title, Date date, String content) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.content = content;
+        this.featured = false;
+        this.modified = date;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(Boolean featured) {
+        this.featured = featured;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Post)) {
+            return false;
+        }
+        Post other = (Post) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "id.ac.itb.informatika.wbd.jpa.entities.Post[ id=" + id + " ]";
+    }
+    
+}
---
+++ SimpleBlog/src/java/id/ac/itb/informatika/wbd/jsf/PostController.java
@@ -0,0 +1,11 @@
+package id.ac.itb.informatika.wbd.jsf;
+
+public class PostController {
+
+    public PostController() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+    }
+    
+    private Post post = null;
+    
+}
