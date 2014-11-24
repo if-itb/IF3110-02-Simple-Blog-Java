8c325eef783909cf3f4a05e00eadf9c43f5a05b7
MarioFilino <mario.filino@gmail.com>
2014-11-24 15:20:39 +0700

    edit publishpost, editpost created

--- SImpleBlog/src/java/Database/EditPost.java
+++ SImpleBlog/src/java/Database/EditPost.java
@@ -6,30 +6,36 @@
 
 package Database;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author Sakurai
  */
-@ManagedBean(name = "Publishpost", eager = true)
-@ViewScoped
-public class PublishPost {          
-    public PublishPost(){
+@ManagedBean(name = "Editpost", eager = true)
+@SessionScoped
+public class EditPost {
+    private int PostID;
+    
+    public EditPost(){
         
     }
+    
+    public void setPostId(int PostID){
+        this.PostID = PostID;
+    }
+    
+    public void Redirect(int PostID){
+        setPostId(PostID);
         
+    }
+    
     private Connection getConnection() throws ClassNotFoundException, SQLException, IllegalAccessException{
         Connection conn = null;
         try{
@@ -45,43 +51,31 @@ public class PublishPost {
         return conn;
     }
     
-    public List<Post> getUnpublishedPost() throws ClassNotFoundException, IllegalAccessException{
+    public Post getPost() throws ClassNotFoundException, SQLException, IllegalAccessException{
         ResultSet result;
-        List<Post> UnpublishedPost = new ArrayList<>();
-        try {
-          Connection conn = getConnection();
-          Statement stmt = conn.createStatement();
-          String query = "Select * from post where Status = \"unpublished\"";
-          result = stmt.executeQuery(query);
-
-          while(result.next()){
-              Post post = new Post();
+        Post post = new Post();
+        try{
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            String query = "Select * From post where ID = " + PostID + ";";
+            result = stmt.executeQuery(query);
+            while(result.next()){
               post.setId(result.getInt(1));
               post.setJudul(result.getString(2));
               post.setTanggal(result.getString(3));
               post.setContent(result.getString(4));
               post.setAuthor(result.getString(5));
               post.setStatus(result.getString(6));
-              UnpublishedPost.add(post);
-           }
-          conn.close();
-        } catch (SQLException e) {
-           System.err.println(e);
+            }
+            conn.close();
+            
+        } catch(SQLException e){
+            System.err.println(e);
         }
-        return UnpublishedPost;
+        
+        return post;
     }
     
-    public void publishPost(int postID) throws ClassNotFoundException, IllegalAccessException{
-        ResultSet result;
-        try {
-          Connection conn = getConnection();
-          Statement stmt = conn.createStatement();
-          String query = "Update post set Status = \"published\" WHERE ID = " + postID + ";";
-          stmt.executeUpdate(query);
-          conn.close();
-        } catch (SQLException e) {
-           System.err.println(e);
-        } 
-    }
+  
     
 }
