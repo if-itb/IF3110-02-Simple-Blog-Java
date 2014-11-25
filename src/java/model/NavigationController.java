fc8eb818716b08990079d3ce8a5e00ee2c1cf1ff
afik <k.afis3@rocketmail.com>
2014-11-25 15:10:43 +0700

    coba database

--- src/java/model/NavigationController.java
+++ src/java/model/NavigationController.java
@@ -38,4 +38,16 @@ public class NavigationController implements Serializable{
     public String gotoAddPost() {
         return "Add-Post.xhtml";
     }
+    
+    public String gotoEditPost(int post_id) {
+        return "Edit-Post.xhtml";
+    }
+    
+    public String gotoDeletePost(int post_id) {
+        return "Delete-Post.xhtml";
+    }
+    
+    public String gotoViewPost(int post_id) {
+        return "View-Post.xhtml";
+    }
 }
