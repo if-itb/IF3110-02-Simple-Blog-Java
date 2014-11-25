87b7be838a3c98377c22cb3de693c89f9c58f58e
wiragotama <wiragotama@gmail.com>
2014-11-25 17:53:29 +0700

    redirect logout

--- src/java/Bean/LogoutBean.java
+++ src/java/Bean/LogoutBean.java
@@ -20,6 +20,6 @@ public class LogoutBean {
     
     public String logout(){
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        return "index.jsf?faces-indirect=true";
+        return "index.jsf?faces-redirect=true";
     }
 }
