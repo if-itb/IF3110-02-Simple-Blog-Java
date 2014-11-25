4fe2aa9b7cb2dc245daef7486d5c999be27a3a0a
ASUS <rita_miffies@yahoo.co.id>
2014-11-25 16:46:15 +0700

    edit

---
+++ TubesSimpleBlog2/src/java/Function/ListPost.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Function;
+
+import java.util.Date;
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Anggi
+ */
+@ManagedBean(name = "listPost", eager = true)
+@RequestScoped
+public class ListPost {
+	private ArrayList<Post> arrPostPublished;
+	private ArrayList<Post> arrPostUnpublished;
+	
+	public ListPost(){
+		arrPostPublished = new ArrayList<>();
+		arrPostUnpublished = new ArrayList<>();
+		String url = "jdbc:mysql://localhost:3306/datapost";
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "root"; 
+		String password = "";
+		try {
+			Class.forName(driver).newInstance();
+			Connection conn = DriverManager.getConnection(url,userName,password);
+			Statement st = conn.createStatement();
+			ResultSet res= st.executeQuery("Select * from posts");
+			Post pos = null;
+			while(res.next()){ 
+				pos = new Post();
+				pos.setIDPost(res.getInt("PID"));
+				pos.setTitle(res.getString("Judul"));
+				pos.setDate(res.getDate("Tanggal"));                
+				pos.setContent(res.getString("Konten"));                
+				pos.setStatus(res.getString("Status"));
+				if (pos.getStatus().equals("published")){
+					arrPostPublished.add(pos);
+				} else {
+					arrPostUnpublished.add(pos);
+				}
+			}
+			conn.close();
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+		}
+	}
+	
+	public ArrayList<Post> getArrPostPublished(){
+		return arrPostPublished;
+	}
+	
+	public ArrayList<Post> getArrPostUnpublished(){
+		return arrPostUnpublished;
+	}
+}
--- TubesSimpleBlog2/src/java/Function/Post.java
+++ TubesSimpleBlog2/src/java/Function/Post.java
@@ -6,8 +6,16 @@
 
 package Function;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -16,13 +24,22 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean(name = "post", eager = true)
 @RequestScoped
 public class Post {
+	@ManagedProperty(value="#{param.idpost}")
+	private int pidToDelete;
 	private int IDPost;
-	private String title = "TITLE COBA";
-	private String content = "HAHAHAA";
-	private String date = "23";
+	private String title;
+	private String content;
+	private Date date;
+	private String status;
 	
-	public Post (){
-		
+	public int getPidToDelete(){
+		return pidToDelete;
+	}
+	public int getIDPost(){
+		return IDPost;
+	}
+	public String getStatus(){
+		return status;
 	}
 	public String getTitle(){
 		return title;
@@ -30,10 +47,10 @@ public class Post {
 	public String getContent(){
 		return content;
 	}
-	public String getDate(){
+	public Date getDate(){
 		return date;
 	}
-	public void setDate(String date){
+	public void setDate(Date date){
 		this.date = date;
 	}
 	public void setTitle(String title){
@@ -42,4 +59,32 @@ public class Post {
 	public void setContent(String content){
 		this.content = content;
 	}
+	public void setStatus(String status){
+		this.status = status;
+	}
+	public void setIDPost(int id){
+		this.IDPost = id;
+	}
+	public void setPidToDelete(int id){
+		this.pidToDelete = id;
+	}
+	
+	public String deletePost(){
+		String url = "jdbc:mysql://localhost:3306/datapost";
+	   String driver = "com.mysql.jdbc.Driver";
+	   String userName = "root"; 
+	   String password = "";
+		try {
+		   Class.forName(driver).newInstance();
+		   Connection conn = DriverManager.getConnection(url,userName,password);
+		   String insertToDB = "delete from posts where PID = ?";
+		   PreparedStatement preparedStatement = conn.prepareStatement(insertToDB);
+		   preparedStatement.setInt(1,pidToDelete);
+		   preparedStatement.executeUpdate();
+		   conn.close();
+		  // FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+	   } catch (ClassNotFoundException /*| IOException*/ | InstantiationException | IllegalAccessException | SQLException e) {
+	   }
+		return "index?faces-redirect=true";
+    }
 }
