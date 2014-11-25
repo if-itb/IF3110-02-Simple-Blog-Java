0114ad1f5243e97611cfc26e71599f6fefc13e43
ichwanhs96 <ichwanharyosembodo96@gmail.com>
2014-11-25 22:41:36 +0700

    progress tuesday 25/11/2014 22:40PM

---
+++ simple-blog/src/java/DatabaseController.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Luqman
+ */
+@ManagedBean(name="databaseController")
+@SessionScoped
+public class DatabaseController implements Serializable {
+    
+    private User user;
+    private DataSource ds;
+    private Context initCtx;
+    private Context envCtx;
+    private Connection conn;
+    private PreparedStatement ps;
+    
+    /**
+     * Creates a new instance of dbTest
+     */
+    public DatabaseController() {
+        System.out.println("db ctrl dibuat");
+    }
+    
+    public User loginValidator(String username, String password){
+        user = new User();
+        try{
+            //get database connection
+            initCtx = new InitialContext();
+            envCtx = (Context) initCtx.lookup("java:comp/env");
+            ds = (DataSource) envCtx.lookup("jdbc/simpleBlogDb");
+
+            Connection conn = ds.getConnection();
+            ps = conn.prepareStatement("SELECT * FROM user WHERE user.username = '" + username +  "' AND user.password = '" + password + "'"); 
+            ResultSet result =  ps.executeQuery();
+            result.next();
+            user.setId(result.getInt("id"));
+            user.setEmail(result.getString("email"));
+            user.setUsername(result.getString("username"));
+            user.setName(result.getString("name"));
+            user.setPassword(result.getString("password"));
+            user.setRole(result.getString("role"));
+
+            conn.close();
+            
+            System.out.println("masuk database");
+            return user;
+        } catch (Exception e) {
+            System.out.println("masuk exception database");
+            user.setRole("guest");
+            return user;
+        }
+    }
+}
