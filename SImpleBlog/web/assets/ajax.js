9f49d90e022996129d46ab39d85b67c6669bf5d1
MarioFilino <mario.filino@gmail.com>
2014-11-25 17:45:48 +0700

    ajax comment selesai, tpi masih aneh

--- SImpleBlog/web/assets/ajax.js
+++ SImpleBlog/web/assets/ajax.js
@@ -6,19 +6,68 @@
 
 
 function LoadCommentList(PostID){
-	var xmlhttp;
+    var xmlhttp;
 
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
-	}else{
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange=function(){
-		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-			document.getElementById("comment-list").innerHTML=xmlhttp.responseText;
-		}
-	}
-	xmlhttp.open("GET","loadcomments.jsp?ID=" + PostID,true);
-	xmlhttp.send();	
+    if (window.XMLHttpRequest){
+            xmlhttp=new XMLHttpRequest();
+    }else{
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function(){
+            if (xmlhttp.readyState==4 && xmlhttp.status==200){
+                    document.getElementById("comment-list").innerHTML=xmlhttp.responseText;
+            }
+    }
+    xmlhttp.open("GET","assets/loadcomments.jsp?ID=" + PostID,true);
+    xmlhttp.send();	
+}
+
+function AddComment(PostID){
+    if(IsEmailValid()){
+        var xmlhttp;
+        if (window.XMLHttpRequest){
+                xmlhttp=new XMLHttpRequest();
+        }else{
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function(){
+                if (xmlhttp.readyState==4 && xmlhttp.status==200){
+                        document.getElementById("comment-list").innerHTML=xmlhttp.responseText;
+                }
+        }
+        xmlhttp.open("POST","assets/newcomment.jsp?ID=" +PostID + "&Name=" + document.getElementById("Nama").value + "&Email=" + document.getElementById("Email").value + "&Comment=" + document.getElementById("Komentar").value,true);
+        xmlhttp.send();	
+        return true;
+    }else{
+        return false;	
+    }
+}
+
+function AddCommentLoggedIn(PostID){
+    var xmlhttp;
+    if (window.XMLHttpRequest){
+            xmlhttp=new XMLHttpRequest();
+    }else{
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function(){
+            if (xmlhttp.readyState==4 && xmlhttp.status==200){
+                    document.getElementById("comment-list").innerHTML=xmlhttp.responseText;
+            }
+    }
+    xmlhttp.open("POST","assets/newcomment.jsp?ID=" +PostID + "&Name=" + document.getElementById("Nama2").value + "&Email=" + document.getElementById("Email2").value + "&Comment=" + document.getElementById("Komentar2").value,true);
+    xmlhttp.send();	
+}
+
+function IsEmailValid(){
+    var regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    var emailInput = document.forms["Form"]["Email"].value;
+
+    if(!regex.test(emailInput)){
+            alert("Invalid email");
+            return false;	
+    }else{
+            return true;	
+    }
 }
 
