63b004c0b2b124e52e133a0479e8107cbc25ed7f
Winson <13512071@std.stei.itb.ac.id>
2014-11-25 12:01:04 +0700

    Delete, Undelete, Publish, and Unpublish Post's Bean

---
+++ src/controller/EditPostStatus.java
@@ -0,0 +1,64 @@
+package controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean
+@RequestScoped
+public class EditPostStatus {
+	private int id;
+
+	public void delete() {
+		DatabaseUtility dbUtil = DatabaseUtility.getInstance();
+
+		String query = "UPDATE post SET id_deleted = 1 WHERE id = "
+				+ this.getId();
+
+		System.out.println(query);
+
+		dbUtil.execute(query);
+	}
+	
+	public void undelete(){
+		DatabaseUtility dbUtil = DatabaseUtility.getInstance();
+
+		String query = "UPDATE post SET id_deleted = 0 WHERE id = "
+				+ this.getId();
+
+		System.out.println(query);
+
+		dbUtil.execute(query);	
+	}
+
+	public void publish() {
+		DatabaseUtility dbUtil = DatabaseUtility.getInstance();
+
+		String query = "UPDATE post SET is_published = 1 WHERE id = "
+				+ this.getId();
+
+		System.out.println(query);
+
+		dbUtil.execute(query);
+	}
+	
+	public void unpublish() {
+		DatabaseUtility dbUtil = DatabaseUtility.getInstance();
+
+		String query = "UPDATE post SET is_published = 0 WHERE id = "
+				+ this.getId();
+
+		System.out.println(query);
+
+		dbUtil.execute(query);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int i) {
+		id = i;
+	}
+	
+}
