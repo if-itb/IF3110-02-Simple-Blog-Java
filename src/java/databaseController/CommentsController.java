230343fd0f70636d7b166fcb3ee6553fd5d4374a
Kevin Maulana <kevhnmay94@gmail.com>
2014-11-27 05:49:37 +0700

    Added beans

---
+++ src/java/databaseController/CommentsController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseController;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author kevhnmay94
+ */
+@ManagedBean
+@RequestScoped
+public class CommentsController {
+
+    /**
+     * Creates a new instance of CommentsController
+     */
+    public CommentsController() {
+    }
+    
+}
