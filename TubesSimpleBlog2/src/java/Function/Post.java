d3b40919cff36759ac381fc94317d222f69f50b5
Linda <sekawati.linda@gmail.com>
2014-11-26 17:27:57 +0700

    Linda Versi 1

--- TubesSimpleBlog2/src/java/Function/Post.java
+++ TubesSimpleBlog2/src/java/Function/Post.java
@@ -10,11 +10,16 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 /**
@@ -22,9 +27,9 @@ import javax.faces.context.FacesContext;
  * @author Anggi
  */
 @ManagedBean(name = "post", eager = true)
-@RequestScoped
+@ViewScoped
 public class Post {
-	@ManagedProperty(value="#{param.idpost}")
+	//@ManagedProperty(value="#{param.idpost}")
 	private int pidToDelete;
 	private int IDPost;
 	private String title;
@@ -68,9 +73,76 @@ public class Post {
 	public void setPidToDelete(int id){
 		this.pidToDelete = id;
 	}
-	
-	public String deletePost(){
-		String url = "jdbc:mysql://localhost:3306/datapost";
+	    
+        public String addPost(){
+            String url = "jdbc:mysql://localhost:3306/datapost";
+            String driver = "com.mysql.jdbc.Driver";
+            String userName = "root"; 
+            String password = "";
+            	try {
+                   java.sql.Date sqlDate= new java.sql.Date(date.getTime());
+                   Class.forName(driver).newInstance();
+		   Connection conn = DriverManager.getConnection(url,userName,password);
+                   String insertToDB = "insert into posts (`Judul`,`Tanggal`,`Konten`,`Status`) value (?,?,?,?)";
+		   PreparedStatement preparedStatement = conn.prepareStatement(insertToDB);
+                   preparedStatement.setString(1, this.title);
+                   preparedStatement.setDate(2, sqlDate);
+		   preparedStatement.setString(3, this.content);
+		   preparedStatement.setString(4, "unpublished");
+		   preparedStatement.executeUpdate();
+		   conn.close();
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+                    }
+                return "unpublished_post?faces-redirect=true";
+        }
+        
+        public void showPost(int id){
+           String url = "jdbc:mysql://localhost:3306/datapost";
+	   String driver = "com.mysql.jdbc.Driver";
+	   String userName = "root"; 
+	   String password = "";
+           try{
+                Class.forName(driver).newInstance();
+		Connection conn = DriverManager.getConnection(url,userName,password);
+                Statement st = conn.createStatement();
+                ResultSet res = st.executeQuery("Select * from posts where pid = " +id);
+                
+                if(res.next()){
+                    title = res.getString("Judul");
+                    date = res.getDate("Tanggal");
+                    content = res.getString("Konten");
+                    IDPost = id;
+                }
+           }catch (ClassNotFoundException /*| IOException*/ | InstantiationException | IllegalAccessException | SQLException e) {
+	   }
+		
+        }
+        
+        public String editPost(){
+            System.out.println("idedit="+IDPost);
+            String url = "jdbc:mysql://localhost:3306/datapost";
+            String driver = "com.mysql.jdbc.Driver";
+            String userName = "root"; 
+            String password = "";
+            	try {
+                   java.sql.Date sqlDate= new java.sql.Date(date.getTime());
+                   Class.forName(driver).newInstance();
+		   Connection conn = DriverManager.getConnection(url,userName,password);
+                   String insertToDB = "update posts set Judul =?, Tanggal =? ,Konten=? where PID=?";
+		   PreparedStatement preparedStatement = conn.prepareStatement(insertToDB);
+                   preparedStatement.setString(1, this.title);
+                   preparedStatement.setDate(2, sqlDate);
+		   preparedStatement.setString(3, this.content);
+                   preparedStatement.setInt(4,IDPost);
+		   preparedStatement.executeUpdate();
+		   conn.close();
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+                    }
+                return "index?faces-redirect=true";
+        
+        }
+	public String deletePost(int id){
+	   String url = "jdbc:mysql://localhost:3306/datapost";
 	   String driver = "com.mysql.jdbc.Driver";
 	   String userName = "root"; 
 	   String password = "";
@@ -79,12 +151,30 @@ public class Post {
 		   Connection conn = DriverManager.getConnection(url,userName,password);
 		   String insertToDB = "delete from posts where PID = ?";
 		   PreparedStatement preparedStatement = conn.prepareStatement(insertToDB);
-		   preparedStatement.setInt(1,pidToDelete);
+		   preparedStatement.setInt(1,id);
 		   preparedStatement.executeUpdate();
 		   conn.close();
 		  // FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
 	   } catch (ClassNotFoundException /*| IOException*/ | InstantiationException | IllegalAccessException | SQLException e) {
 	   }
 		return "index?faces-redirect=true";
-    }
+        }
+     public String changeStatus(int id){
+            String url = "jdbc:mysql://localhost:3306/datapost";
+            String driver = "com.mysql.jdbc.Driver";
+            String userName = "root"; 
+            String password = "";
+            try {
+                Class.forName(driver).newInstance();
+                Connection conn = DriverManager.getConnection(url,userName,password);
+                
+                  String insertToDB = "update posts set Status='published' where PID=?";
+                  PreparedStatement preparedStatement = conn.prepareStatement(insertToDB);
+		  preparedStatement.setInt(1,id);
+		  preparedStatement.executeUpdate();
+		  conn.close();
+             } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+                    }
+            return "unpublished_post?faces-redirect=true";
+     }
 }
