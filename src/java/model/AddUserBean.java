f7e2c32586331238aa5e7eaf62cb66900d3e36a2
calvinsadewa <master23680@gmail.com>
2014-11-26 07:40:19 +0700

    Penambahan validator dan perbaikan list user

---
+++ src/java/model/AddUserBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author calvin-pc
+ */
+public class AddUserBean {
+    // TODO semua, sambungin ke List-User.xhtml, dao, dan teman - teman
+    private UserBean user;
+    /**
+     * Creates a new instance of AddUserBean
+     */
+    public AddUserBean() {
+        user = new UserBean();
+    }
+
+    /**
+     * @return the user
+     */
+    public UserBean getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(UserBean user) {
+        this.user = user;
+    }
+    
+    public void add() {
+        // TODO Sambungin ke database
+    }
+}
