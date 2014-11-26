e6530b3788f63029b9182c54faf5a6a6c443e3e4
runbow <dragoon.redeyed@gmail.com>
2014-11-26 16:24:55 +0700

    repair logout link

---
+++ src/java/paket/CookieCheck.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paket;
+
+import java.util.Map;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class CookieCheck {
+
+    FacesContext facesContext = FacesContext.getCurrentInstance();
+    HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+    
+    /**
+     * Creates a new instance of CookieCheck
+     */
+    public String CookieCheck() {
+        String cValue=null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if("role".equals(cookie.getName())){
+                cValue = cookie.getValue();
+            }
+        }
+        return cValue;
+    }
+    
+    public boolean isLoggedIn(){
+        return CookieCheck()!=null;
+    }
+    
+    public boolean isAdmin(){
+        return "admin".equals(CookieCheck());
+    }
+    
+    public String getUName(){
+        String UName="Guest";
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if("user".equals(cookie.getName())){
+                UName = cookie.getValue();
+            }
+        }
+        return UName;
+    }
+}
