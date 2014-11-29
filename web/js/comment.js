2d774942009cb12091d4dbf8d92ed1f8dc32f0b0
Riva Syafri Rachmatullah <riva.syafri.r@gmail.com>
2014-11-30 01:01:04 +0700

    stuck

---
+++ web/js/comment.js
@@ -0,0 +1,85 @@
+function addComment() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var content = document.getElementById("content").value;
+    if ((name != "") && (email != "") && (content != "")) {
+        if (validateEmail(email)) {
+            var xmlhttp;
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            } else {
+                // code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange=function() {
+                if (xmlhttp.readyState<4) {
+                    document.getElementById("submit").style="display:none";
+                    document.getElementById("comment_list").style="display:none";
+                } else if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    if (xmlhttp.responseText == "true") {
+                        loadComment();
+                        document.getElementById("comment_form").reset();
+                    } else {
+                        alert("Fail to process");
+                        document.getElementById("submit").style="display:block";
+                    }
+                }
+            }
+            xmlhttp.open("POST","controller/addComment.php",true);
+            xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xmlhttp.send("id=param['id']&nama="+name+"&email="+email+"&content="+content);
+        } else {
+            alert("Invalid email!");
+            document.getElementById("email").focus();
+        }
+    } else {
+        alert("Invalid input");
+        document.getElementById("submit").style="display:block";
+    }
+}
+function loadComment() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState<4) {
+            document.getElementById("submit").style="display:none";
+            document.getElementById("comment_list").style="display:none";
+        } else if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            if (xmlhttp.responseText != "zero") {
+                var list = JSON.parse(xmlhttp.responseText);
+                var inner = "";
+                var row;
+                var i = 0;
+                for (row in list) {
+                    inner += '<!-- Comment -->';
+                    inner += '      <div class="media">';
+                    inner += '          <a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/64x64" alt="" /></a>';
+                    inner += '          <div class="media-body">';
+                    inner += '              <h4 class="media-heading">'+list[i].name+'<small>'+list[i].time+'</small></h4>';
+                    inner += '              ' + list[i].content;
+                    inner += '          </div>';
+                    inner += '      </div>';
+                    i++;
+                }
+                document.getElementById("comment_list").innerHTML = inner;
+            }
+            document.getElementById("submit").style="display:block";
+            document.getElementById("comment_list").style="display:block";
+        }
+    }
+    xmlhttp.open("POST","controller/get_komen.php",true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send("id=param['id']");
+}
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+loadComment();
