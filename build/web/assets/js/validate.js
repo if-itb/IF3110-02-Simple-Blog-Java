a56ba9ecddd13b8a6b4c7c9d72a8a6e34086a31c
kevinyu <kevin.kayu@gmail.com>
2014-12-01 22:46:09 +0700

    Add soft delete and fix editor functionality

--- build/web/assets/js/validate.js
+++
@@ -1,8 +0,0 @@
-function validateDeletion() {
-/*	Post deletion confirmation */
-	var answer = confirm ("Are you sure want to delete this user?");
-	if (answer) {
-		return true;
-	}
-	else return false;
-}
