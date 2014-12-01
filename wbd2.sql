ddf762c7ca23d0c79c285a6dada97f1910745e29
luthfi <luthfi@IIT-HMIF>
2014-12-02 03:00:50 +0700

    Dump new SQL

--- wbd2.sql
+++ wbd2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.2.5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2014 at 05:35 PM
--- Server version: 5.5.40
--- PHP Version: 5.6.0
+-- Generation Time: Dec 02, 2014 at 04:00 AM
+-- Server version: 5.5.38
+-- PHP Version: 5.4.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -33,15 +33,16 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `email` varchar(30) NOT NULL,
   `content` text NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `comment`
 --
 
 INSERT INTO `comment` (`id`, `pid`, `name`, `email`, `content`, `time`) VALUES
-(1, 1, 'riva', 'riva@riva.com', 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.', '2014-11-29 04:58:47'),
-(2, 1, 'luthfi', 'luthfi@luthfi.com', 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.', '2014-11-29 04:58:47');
+(3, 9, 'admin', 'admin@simple-blog.com', 'Test', '2014-12-01 03:50:19'),
+(4, 10, 'Luthfi Hamid Masykuri', 'luthfi_hamid_m@yahoo.co.id', 'ndsdjnsjd', '2014-12-01 03:51:44'),
+(5, 9, 'Luthfi', 'luthfi_hamid_m@arc.itb.ac.id', 'hahah', '2014-12-01 03:53:53');
 
 -- --------------------------------------------------------
 
@@ -52,21 +53,23 @@ INSERT INTO `comment` (`id`, `pid`, `name`, `email`, `content`, `time`) VALUES
 CREATE TABLE IF NOT EXISTS `post` (
 `id` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL,
   `date` date NOT NULL,
   `content` varchar(1000) NOT NULL,
   `author` varchar(50) NOT NULL,
   `published` tinyint(1) NOT NULL,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `post`
 --
 
-INSERT INTO `post` (`id`, `title`, `date`, `content`, `author`, `published`, `deleted`) VALUES
-(1, 'Artikel', '2014-11-25', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\r\n', 'admin', 1, 0),
-(2, 'Artikel', '2014-11-26', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', 'luthfi', 1, 0),
-(3, 'Artikel kita', '2014-11-26', 'vewfwe', 'admin', 1, 0);
+INSERT INTO `post` (`id`, `title`, `image`, `date`, `content`, `author`, `published`, `deleted`) VALUES
+(9, 'Test123', 'admin_2014-12-01_10-35-51.61.jpg', '2014-12-01', 'vcerjh 4ewfiherufnvjern', 'admin', 0, 1),
+(10, 'Coba', 'luthfi_2014-12-01_10-51-28.036.jpg', '2014-12-02', 'fweijfiweufhnjwenf e', 'luthfi', 1, 0),
+(12, 'Coba lagi', 'admin_2014-12-02_01-15-21.499.jpg', '2014-12-02', 'Lorem ipsum dolor sit amet, enim oblique detracto eu eum, eum consulatu democritum interpretaris ad, usu no impedit scribentur. Libris repudiare nam in, audire expetendis mea ad, at eum choro equidem consequuntur. Te eum semper denique tractatos. Eu cum scaevola abhorreant. Bonorum recteque quaerendum mei ne.', 'admin', 0, 0),
+(13, 'Test', 'admin_2014-12-02_02-23-41.815.jpg', '2014-12-01', 'Test', 'admin', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -87,8 +90,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `role`, `name`, `email`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin', 'admin@simple-blog.com'),
-('luthfi', 'd5cd72b7bcbf56bc503904f1ac7d9bc2', 'owner', 'Luthfi Hamid Masykuri', 'luthfi_hamid_m@yahoo.co.id');
+('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'MiminLab', 'admin@simple-blog.com'),
+('luthfi', 'd5cd72b7bcbf56bc503904f1ac7d9bc2', 'owner', 'Luthfi Hamid Masykuri', 'luthfi_hamid_m@yahoo.co.id'),
+('riva', 'ad9588ec76eb97853f9ee03ef1dc80a7', 'editor', 'riva', 'riav@riva.com');
 
 --
 -- Indexes for dumped tables
@@ -120,12 +124,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- Constraints for dumped tables
 --
