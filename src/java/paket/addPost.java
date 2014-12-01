21f3c999fa254058e6e876a608394d3ee9ead4bc
Mario Tressa Juzar <mariotj.tj@gmail.com>
2014-12-01 16:44:36 +0700

    tambah add post
    tambah add post

--- src/java/paket/addPost.java
+++ src/java/paket/addPost.java
@@ -7,136 +7,94 @@
 package paket;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author Mario
  */
-@ManagedBean(name = "addPost", eager=true)
+@ManagedBean
 @RequestScoped
-public class addPost extends HttpServlet {
-
-    String dbURL = "jdbc:mysql://localhost:3306/simple_blog";
-    String uName = "root";
-    String pass = "";
-    String username, judul, tanggal, konten, tgl, bln, thn;
+public class addPost {
     
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    public void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
+    private String tanggal, konten, judul;
+    private String username;
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+    
+    public void ambilTanggal(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        this.tanggal = dateFormat.format(date);
+    }
+
+    public String getKonten() {
+        return konten;
+    }
+
+    public void setKonten(String konten) {
+        this.konten = konten;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+    
+    public String addPost() throws IOException{
+        String dbURL = "jdbc:mysql://localhost:3306/simple_blog";
+        String uName = "root";
+        String pass = "";
         
-        PrintWriter out = response.getWriter();
         Connection conn = null;
         Statement stmnt = null;
         
-        HttpSession session = request.getSession();
-        username = session.getValue("user").toString();
+        CookieCheck cookie = new CookieCheck();
+        username = cookie.getUsrName();
+        
+        System.out.print(username);
+        System.out.print("username sebelah sini <<");
+        System.out.print(tanggal);
+        System.out.print(judul);
+        System.out.print(konten);
         
         try {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-            } catch (Exception e) {
-                out.println("Unable to load Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("Unable to load Driver");
             }
             conn = DriverManager.getConnection(dbURL, uName, pass);
             stmnt = conn.createStatement();
             
-            judul = request.getParameter("judul");
-            konten = request.getParameter("konten");
-            tanggal = request.getParameter("tangal");
+            String sqlStr = "INSERT INTO `post`(`judul`,`konten`,`tanggal_post`,`usrname`) VALUE('" +judul +"', '" +konten +"', '" +tanggal +"', '" +username +"')";
+            stmnt.executeUpdate(sqlStr);
             
-            String sqlstr = "INSERT INTO `post`(`judul`, `konten`, `usrname`) VALUES('" + judul + "', '" + konten + "', '" + username + "') ";
-            stmnt.executeQuery(sqlstr);
-            response.sendRedirect("index.html");
+            FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
             
-        } catch (SQLException ex) {
-            out.println("Unable to connect to database");
-            out.print(ex.toString());
+        } catch (SQLException e){
             
-            
-        }finally {
-            out.close();
-            try {
-                // Step 5: Close the Statement and Connection
-                if (stmnt != null) {
-                    stmnt.close();
-                }
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-            }
         }
-        
+        return null;
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(addPost.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(addPost.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
 }
---
+++ src/java/paket/editPost.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paket;
+
+/**
+ *
+ * @author Mario
+ */
+public class editPost {
+
+    /**
+     * Creates a new instance of editPost
+     */
+    public editPost() {
+    }
+    
+}
